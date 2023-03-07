739e8c628f2a83b349a75855ed07f49c07cd5148
oleg.kuzmin <oleg1kuzmin@gmail.com>
2023-03-07 21:39:12 +0200

    flasktaskr finished first part

--- flasktaskr/_config.py
+++ flasktaskr/_config.py
@@ -1,10 +1,11 @@
 import os
 
+# grab the folder where this script lives
 basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'myprecious'
-
-DATABASE_PATH = os.past.join(basedir, DATABASE)
+# define the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
